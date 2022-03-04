3ec0a5a35974bdecb55c6f671312b16b7717ce4f
0xSmartCrypto <0xSmartCrypto@gmail.com>
2022-03-04 21:01:56 +0800

    mints domains as nft

--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,19 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-  const domainContract = await domainContractFactory.deploy("ninja");
+  const domainContract = await domainContractFactory.deploy("anon");
   await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
   console.log("Contract deployed by:", owner.address);
   
   const txn = await domainContract.register(
-    "super", 
+    "not", 
     { value: hre.ethers.utils.parseEther("0.1") }
   );
   await txn.wait();
 
   const domainOwner = await domainContract.getAddress("super");
-  console.log("Owner of domain super.ninja:", domainOwner);
+  console.log("Owner of domain super.anon:", domainOwner);
 
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Balance of domain contract:", hre.ethers.utils.formatEther(balance));
